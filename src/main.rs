9acc3e0d5e95f4bd62ff3f30128981ced1d29d46
Abhishek Sunil <abhisheksunil92@gmail.com>
2024-04-02 13:55:07 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::net::TcpListener;
+
+fn main() {
+    println!("Logs from your program will appear here!");
+
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
